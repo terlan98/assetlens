72a1db7a1649f7862242adccfd3c0c6137236353
Tarlan Ismayilsoy <itarlan@azercell.com>
2025-08-10 22:12:07 +0400

    Improve potential savings computation

--- AssetLens/Model/SimilarityGroup.swift
+++ AssetLens/Model/SimilarityGroup.swift
@@ -11,11 +11,18 @@ struct SimilarityGroup {
     let primary: ImageAsset
     let similar: [(ImageAsset, Float)]
     
+    /// All assets in the group (primary + similar)
+    var allAssets: [ImageAsset] {
+        [primary] + similar.map { $0.0 }
+    }
+    
     var totalSize: Int64 {
         primary.fileSize + similar.reduce(0) { $0 + $1.0.fileSize }
     }
     
     var potentialSavings: Int64 {
-        similar.reduce(0) { $0 + $1.0.fileSize }
+        let totalSize = allAssets.reduce(0) { $0 + $1.fileSize }
+        let smallestAssetSize = allAssets.map { $0.fileSize }.min() ?? 0
+        return totalSize - smallestAssetSize
     }
 }
